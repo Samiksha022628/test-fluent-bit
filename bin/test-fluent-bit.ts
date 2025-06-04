8ea2aac0bb25d793f7a2d47222d374436882919f
Samiksha022628 <samiksha022628@gmail.com>
2025-06-04 10:11:40 +0530

    test

--- bin/test-fluent-bit.ts
+++ bin/test-fluent-bit.ts
@@ -14,7 +14,7 @@ new DeployingMicoserviceOnEksStack(app, 'DeployingMicoserviceOnEksStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '975050071559', region: 'us-east-1' },
+  env: { account: '471112992067', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/test-fluent-bit-stack.ts
+++ lib/test-fluent-bit-stack.ts
@@ -8,8 +8,9 @@ import * as path from 'path';
 import { KubectlV28Layer } from '@aws-cdk/lambda-layer-kubectl-v28';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-export class DeployingMicoserviceOnEksStack extends cdk.Stack{
-  constructor(scope:Construct, id:string, props?:cdk.StackProps) {super(scope,id,props);
+export class DeployingMicoserviceOnEksStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
     const envconfigs = this.node.tryGetContext('envconfigs');
 
@@ -17,95 +18,134 @@ export class DeployingMicoserviceOnEksStack extends cdk.Stack{
       assumedBy: new iam.AccountRootPrincipal(),
     });
 
-   const vpc=new ec2.Vpc(this,'vpc',{
+    const vpc = new ec2.Vpc(this, 'vpc', {
       natGateways: 1,
       subnetConfiguration: [
-        {name: 'PrivateSubnet', subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS, cidrMask: 24,},
-        {name: 'PublicSubnet', subnetType: ec2.SubnetType.PUBLIC, cidrMask: 24,},
+        {
+          name: 'PrivateSubnet',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
       ],
     });
 
-    const cluster=new eks.Cluster(this, 'EksCluster', 
-        {clusterName: 'EksCluster',
-          defaultCapacity:0,
-          vpc,
-          version: eks.KubernetesVersion.V1_28,
-          kubectlLayer: new KubectlV28Layer(this, 'kubectl'),
-          vpcSubnets:[{subnetType:ec2.SubnetType.PRIVATE_WITH_EGRESS}],
-          mastersRole:iamroleforcluster,
-           })
-            
-        const nodegroup=cluster.addNodegroupCapacity('NodeGroup',{
-        desiredSize:2,
-        instanceTypes: [new ec2.InstanceType('t3.medium')],
-        remoteAccess: { sshKeyName: 'demo',
-        },
-      });
+    const cluster = new eks.Cluster(this, 'EksCluster', {
+      clusterName: 'EksCluster',
+      defaultCapacity: 0,
+      vpc,
+      version: eks.KubernetesVersion.V1_28,
+      kubectlLayer: new KubectlV28Layer(this, 'kubectl'),
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
+      mastersRole: iamroleforcluster,
+    });
 
-      nodegroup.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName
-        ('AmazonSSMManagedInstanceCore'));
-      
-      cluster.awsAuth.addRoleMapping(nodegroup.role, {
-        username: 'system:node:{{EC2PrivateDNSName}}',
-        groups: ['system:bootstrappers', 'system:nodes', 'system:masters'],
-     });
+    const nodegroup = cluster.addNodegroupCapacity('NodeGroup', {
+      desiredSize: 2,
+      instanceTypes: [new ec2.InstanceType('t3.medium')],
+      remoteAccess: { sshKeyName: 'demo' },
+    });
 
-      cluster.addHelmChart('MetricsServer', {
-        chart: 'metrics-server',
-        repository: 'https://kubernetes-sigs.github.io/metrics-server/',
-        release: 'metrics-server',
-        namespace: 'kube-system',
-        values: {args: [
-        '--kubelet-insecure-tls',
-        '--kubelet-preferred-address-types=InternalIP,Hostname,ExternalIP',],},
-      });
+    nodegroup.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        'AmazonSSMManagedInstanceCore'
+      )
+    );
 
-      const fluentBitNamespace = 'amazon-cloudwatch';
+    cluster.awsAuth.addRoleMapping(nodegroup.role, {
+      username: 'system:node:{{EC2PrivateDNSName}}',
+      groups: ['system:bootstrappers', 'system:nodes', 'system:masters'],
+    });
 
-      const fluentBitSA = cluster.addServiceAccount('FluentBitSA', {
-        name: 'fluent-bit',
-        namespace: fluentBitNamespace,
-      });
+    cluster.addHelmChart('MetricsServer', {
+      chart: 'metrics-server',
+      repository: 'https://kubernetes-sigs.github.io/metrics-server/',
+      release: 'metrics-server',
+      namespace: 'kube-system',
+      values: {
+        args: [
+          '--kubelet-insecure-tls',
+          '--kubelet-preferred-address-types=InternalIP,Hostname,ExternalIP',
+        ],
+      },
+    });
 
-      fluentBitSA.addToPrincipalPolicy(new iam.PolicyStatement({
-        actions: [
-        'logs:PutLogEvents',
-        'logs:CreateLogStream',
-        'logs:CreateLogGroup',
-        'logs:DescribeLogStreams',
-      ],
-    resources: ['*'], 
-    }));
+    const fluentBitNamespace = 'amazon-cloudwatch';
 
     const fluentBitManifestsDir = path.join(__dirname, '../manifests/fluent-bit');
-    const fluentBitFiles = ['namespace-cloudwatch.yaml', 'service-account.yaml', 'configmap.yaml', 'daemon-set.yaml'];
+    const fluentBitFiles = [
+      'namespace-cloudwatch.yaml',
+      'service-account.yaml',
+      'configmap.yaml',
+      'daemon-set.yaml',
+    ];
 
-const loadYamlManifest = (fileName: string) =>
-  yaml.parseAllDocuments(fs.readFileSync(path.join(fluentBitManifestsDir, fileName), 'utf8'))
-    .map(doc => doc.toJSON())
-    .filter(Boolean);
+    const loadYamlManifest = (fileName: string) =>
+      yaml
+        .parseAllDocuments(
+          fs.readFileSync(path.join(fluentBitManifestsDir, fileName), 'utf8')
+        )
+        .map((doc) => doc.toJSON())
+        .filter(Boolean);
 
-const namespaceManifest = loadYamlManifest('namespace-cloudwatch.yaml');
+    const namespaceManifest = loadYamlManifest('namespace-cloudwatch.yaml');
+    const fluentBitNs = cluster.addManifest('FluentBitNamespace', ...namespaceManifest);
 
-const fluentBitOtherResources = fluentBitFiles
-  .filter(f => f !== 'namespace-cloudwatch.yaml') 
-  .flatMap(file => {
-    if (file === 'service-account.yaml') {
-      const saContent = fs.readFileSync(path.join(fluentBitManifestsDir, file), 'utf8')
-        .replace('<IRSA_ROLE_ARN_PLACEHOLDER>', fluentBitSA.role.roleArn);
-      return yaml.parseAllDocuments(saContent).map(doc => doc.toJSON()).filter(Boolean);
-    }
-    return loadYamlManifest(file);
-  });
+    const fluentBitSA = cluster.addServiceAccount('FluentBitSA', {
+      name: 'fluent-bit',
+      namespace: fluentBitNamespace,
+    });
+    fluentBitSA.node.addDependency(fluentBitNs); 
 
-      const fluentBitNs = cluster.addManifest('FluentBitNamespace', ...namespaceManifest);
-      const fluentBitResources = cluster.addManifest('FluentBitResources', ...fluentBitOtherResources);
-      fluentBitResources.node.addDependency(fluentBitNs);
+    fluentBitSA.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'logs:PutLogEvents',
+          'logs:CreateLogStream',
+          'logs:CreateLogGroup',
+          'logs:DescribeLogStreams',
+        ],
+        resources: ['*'],
+      })
+    );
 
-      const manifestsDir = path.join(__dirname, '../manifests');
-      const files =['namespace.yaml','rolebinding.yaml','configMap-secret.yaml','deployment.yaml', 'HPA.yaml', 'job.yaml'];
+    const fluentBitOtherResources = fluentBitFiles
+      .filter((f) => f !== 'namespace-cloudwatch.yaml')
+      .flatMap((file) => {
+        if (file === 'service-account.yaml') {
+          const saContent = fs
+            .readFileSync(path.join(fluentBitManifestsDir, file), 'utf8')
+            .replace('<IRSA_ROLE_ARN_PLACEHOLDER>', fluentBitSA.role.roleArn);
+          return yaml
+            .parseAllDocuments(saContent)
+            .map((doc) => doc.toJSON())
+            .filter(Boolean);
+        }
+        return loadYamlManifest(file);
+      });
 
- for (const envName of Object.keys(envconfigs)) {
+    const fluentBitResources = cluster.addManifest(
+      'FluentBitResources',
+      ...fluentBitOtherResources
+    );
+    fluentBitResources.node.addDependency(fluentBitNs); 
+    fluentBitResources.node.addDependency(fluentBitSA); 
+
+    const manifestsDir = path.join(__dirname, '../manifests');
+    const files = [
+      'namespace.yaml',
+      'rolebinding.yaml',
+      'configMap-secret.yaml',
+      'deployment.yaml',
+      'HPA.yaml',
+      'job.yaml',
+    ];
+
+    for (const envName of Object.keys(envconfigs)) {
       const config = envconfigs[envName];
 
       const placeholders: Record<string, string> = {
@@ -114,28 +154,46 @@ const fluentBitOtherResources = fluentBitFiles
         '{{REPLICA_COUNT}}': (config.replicaCount || 1).toString(),
         '{{REQUEST_CPU}}': config.requestCpu || '100m',
         '{{LIMIT_CPU}}': config.limitCpu || '200m',
-        '{{FEATURE_FLAG}}': config.featureFlag === undefined ? 'false' : config.featureFlag.toString(),
+        '{{FEATURE_FLAG}}':
+          config.featureFlag === undefined
+            ? 'false'
+            : config.featureFlag.toString(),
       };
-    
+
       const replacePlaceholders = (content: string) => {
         for (const [key, value] of Object.entries(placeholders)) {
           content = content.replace(new RegExp(key, 'g'), value);
         }
-          return content;
-       };
+        return content;
+      };
 
       const allResources = files.flatMap((file) => {
-        const content = replacePlaceholders(fs.readFileSync(path.join(manifestsDir, file), 'utf8')
+        const content = replacePlaceholders(
+          fs.readFileSync(path.join(manifestsDir, file), 'utf8')
         );
-        return yaml.parseAllDocuments(content).map((doc) => doc.toJSON()).filter(Boolean);
+        return yaml
+          .parseAllDocuments(content)
+          .map((doc) => doc.toJSON())
+          .filter(Boolean);
       });
 
-      const namespaceResources = allResources.filter((res) => res.kind === 'Namespace');
-      const otherResources = allResources.filter((res) => res.kind !== 'Namespace');
+      const namespaceResources = allResources.filter(
+        (res) => res.kind === 'Namespace'
+      );
+      const otherResources = allResources.filter(
+        (res) => res.kind !== 'Namespace'
+      );
 
-      const namespaceManifest = cluster.addManifest(`NamespaceManifest-${envName}`,...namespaceResources);
-      const appManifest = cluster.addManifest(`AppManifests-${envName}`,...otherResources);
+      const namespaceManifest = cluster.addManifest(
+        `NamespaceManifest-${envName}`,
+        ...namespaceResources
+      );
+      const appManifest = cluster.addManifest(
+        `AppManifests-${envName}`,
+        ...otherResources
+      );
 
       appManifest.node.addDependency(namespaceManifest);
     }
-  }}
+  }
+}
